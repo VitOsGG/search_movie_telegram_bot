befc28188df533c31467edebf4ac8ce1d2204f7c
Viktor Pupkov <ITDSVP22@gmail.com>
2022-12-26 22:28:49 +0400

    Add new large assets

--- create_bot.py
+++ create_bot.py
@@ -3,5 +3,7 @@ from aiogram.dispatcher import Dispatcher
 import os
 
 
-bot = Bot(token=os.getenv('TOKEN'))
+"""Запуск бота только для локальной машины через bat-файл"""
+bot = Bot(os.getenv('TOKEN'))
+
 dp = Dispatcher(bot)
